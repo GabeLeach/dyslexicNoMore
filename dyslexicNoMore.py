0fca178e7eda2bc8e589c41c7851530cc15636a3
G_Leach <leach.gabriel98@gmail.com>
2022-11-03 10:30:16 +0000

    Create dyslexicNoMore.py
    Basic pseudo and description

---
+++ dyslexicNoMore.py
@@ -0,0 +1,18 @@
+#A small app to enable the parsing of text into a more dyslexic friendly format.
+#Concept is to allow user to drop in file and then we parse that and embolden the first couple of letters of each word.
+
+
+#Create  markdown
+#parse input into markdown
+#Format in text file
+#output back to the user
+#once functionality is complete, create a nice UI
+
+#for i in text:
+    #if i.length <=2 bold first char
+    #if i.length == 3 bold first 2 char
+    #else bold 3 chars
+
+def dyslexicNoMore(text):
+    newtext = text
+
